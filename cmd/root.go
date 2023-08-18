7edb234ff7af09940a8ae625d9f3f250d670bf63
NakuRei <nakurei7901@gmail.com>
2023-08-18 09:39:45 +0900

    :sparkles: Add new flag "greeting"

--- cmd/root.go
+++ cmd/root.go
@@ -28,7 +28,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-
+var greeting string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -43,7 +43,7 @@ to quickly create a Cobra application.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Hello World!")
+		fmt.Println(greeting, "World!")
 	},
 }
 
@@ -57,13 +57,5 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.go-cli-hello-world.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.Flags().StringVarP(&greeting, "greeting", "g", "Hello", "Greeting message to display before 'World!'")
 }
