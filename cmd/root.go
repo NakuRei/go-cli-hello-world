3e1db28ae6f8806ed8cd2ef9d55553b91ed2a7eb
NakuRei <nakurei7901@gmail.com>
2023-08-18 12:57:30 +0900

    :recycle: Change description of CLI

--- cmd/root.go
+++ cmd/root.go
@@ -33,13 +33,9 @@ var greeting string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "go-cli-hello-world",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Display 'Hello World!'",
+	Long: `Display "'Hello World!'
+You can change the greeting from 'Hello'`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println(greeting, "World!")
 	},
